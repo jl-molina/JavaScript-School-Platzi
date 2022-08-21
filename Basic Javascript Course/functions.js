1a06d611f395c3d717458b9f88f678d7abad9bbc
jl-molina <joseluisml1218@gmail.com>
2022-08-21 14:49:55 -0600

    variables and functions class

---
+++ Basic Javascript Course/functions.js
@@ -0,0 +1,11 @@
+// Function declaration
+function myFunction(){
+    return 1;
+}
+
+// Function expression
+var add = function(a, b) {
+    return a + b;
+}
+
+console.log(myFunction(1,4));
---
+++ Basic Javascript Course/variables.js
@@ -0,0 +1,7 @@
+// declarar
+var edad;
+
+// inicializar
+edad = 25;
+
+console.log(edad);
